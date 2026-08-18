510113b86e92fe64fa5694414e7241ccbbf558cf
John Belles <qualify155@gmail.com>
2026-08-18 10:29:23 -0400

    feat: expose phase 2 navigation modules

---
+++ src/navigation/index.js
@@ -0,0 +1,3 @@
+export { createStrategicMap } from './strategicMap.js';
+export { buildTerritoryIndex, factionColor } from './territories.js';
+export { createLabel, setLabelState } from './labels.js';
